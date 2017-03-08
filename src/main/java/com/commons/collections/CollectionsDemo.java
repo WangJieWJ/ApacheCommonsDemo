3e941baccc2d3686030a3c16a76d860118063096
wangjie <1152689483@qq.com>
2017-03-08 21:59:19 +0800

    添加git忽略配置文件

--- src/main/java/com/commons/collections/CollectionsDemo.java
+++ src/main/java/com/commons/collections/CollectionsDemo.java
@@ -1,9 +1,16 @@
 package com.commons.collections;
 
+import org.apache.commons.collections.Bag;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Title:
  * Description: 测试commons-collections中的集合操作
- *
+ * <p>
  * Project: ApacheCommonsDemo
  * Create User: 王杰
  * Create Time: 2017/3/8
@@ -11,6 +18,13 @@ package com.commons.collections;
 public class CollectionsDemo {
 
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * 测试内置转换器
+     */
+    public static void Transformer(){
 
     }
 }
